b14c294451f31e2f3989e12e5e1aba587ad05810
Haedri <haedri@gmail.com>
2017-05-16 21:08:31 +0200

    Update LibGdx 1.9 - new empty project

---
+++ desktop/src/org/microbasic/sm/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.microbasic.sm.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.microbasic.sm.ShadowMapping;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ShadowMapping(), config);
+	}
+}
